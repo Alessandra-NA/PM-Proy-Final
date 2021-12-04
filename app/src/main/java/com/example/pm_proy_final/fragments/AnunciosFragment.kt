2e46b3958c882db5ebafbea1443a44e985ae74fd
AlerSarmiento <sarmiento.alej@gmail.com>
2021-12-04 08:52:47 -0500

    pantalla principal

--- app/src/main/java/com/example/pm_proy_final/fragments/AnunciosFragment.kt
+++ app/src/main/java/com/example/pm_proy_final/fragments/AnunciosFragment.kt
@@ -4,14 +4,50 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.pm_proy_final.R
 
-class AnunciosFragment: Fragment() {
+class AnunciosFragment: Fragment(), OnItemSelectedListener {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.fragment_anuncios, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val distritos = view.findViewById<Spinner>(R.id.distrito)
+
+
+        var adapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.distritos,
+            android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        distritos.adapter=adapter;
+        distritos.onItemSelectedListener=this
+
+
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        var choice = parent!!.getItemAtPosition(position).toString()
+        Toast.makeText(requireContext(), choice, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
 }
