b2d8a6a53616611a2ffe6bad7ce900daea67ac3a
Ale <alessandra.na001@gmail.com>
2021-12-04 01:16:45 -0500

    avance

--- app/src/main/java/com/example/pm_proy_final/SignupActivity.kt
+++ app/src/main/java/com/example/pm_proy_final/SignupActivity.kt
@@ -9,28 +9,31 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.pm_proy_final.managers.UsuarioManager
 
 class SignupActivity: AppCompatActivity() {
-    private lateinit var txtUsername : String
-    private lateinit var txtPassword : String
-    private lateinit var txtNombres : String
-    private lateinit var txtApellidos : String
+    private lateinit var txtUsername : EditText
+    private lateinit var txtPassword : EditText
+    private lateinit var txtNombres : EditText
+    private lateinit var txtApellidos : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         // declarando variables
-        txtUsername = findViewById<EditText>(R.id.edTxtUsername2).text.toString()
-        txtPassword = findViewById<EditText>(R.id.edtxtPwd2).text.toString()
-        txtNombres = findViewById<EditText>(R.id.edTxtNombres).text.toString()
-        txtApellidos = findViewById<EditText>(R.id.edTxtApellidos).text.toString()
+        txtUsername = findViewById<EditText>(R.id.edTxtUsername2)
+        txtPassword = findViewById<EditText>(R.id.edtxtPwd2)
+        txtNombres = findViewById<EditText>(R.id.edTxtNombres)
+        txtApellidos = findViewById<EditText>(R.id.edTxtApellidos)
+
         findViewById<Button>(R.id.btnRegistro2).setOnClickListener{
-            if(txtUsername == "" || txtPassword == "" || txtNombres == "" || txtApellidos == "") Toast.makeText(this,"Debe completar todos los campos", Toast.LENGTH_SHORT).show()
-            else {
-                val intent : Intent = Intent()
-                intent.setClass(this, LoginActivity::class.java)
-                UsuarioManager().addUsuario(txtUsername,txtPassword,txtNombres,txtApellidos)
-                Toast.makeText(this,"Registro exitoso",Toast.LENGTH_SHORT).show()
-                startActivity(intent)
+            UsuarioManager().verificarExisteUsuario(txtUsername.text.toString()){ yaExiste ->
+                if(txtUsername.text.toString() == "" || txtPassword.text.toString() == "" || txtNombres.text.toString() == "" || txtApellidos.text.toString() == "") Toast.makeText(this,"Debe completar todos los campos", Toast.LENGTH_SHORT).show()
+                else if (yaExiste) Toast.makeText(this,"El usuario ya existe",Toast.LENGTH_SHORT).show()
+                else {
+                    val intent : Intent = Intent()
+                    intent.setClass(this, LoginActivity::class.java)
+                    UsuarioManager().addUsuario(txtUsername.text.toString(),txtPassword.text.toString(),txtNombres.text.toString(),txtApellidos.text.toString())
+
+                    startActivity(intent)
+                }
             }
         }
-        // TODO: 3/12/2021 Verificar usuario existente 
     }
 }
